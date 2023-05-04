7963fbc8d0d3ef933b84fa5bd5c1af6b2665db8f
m.mathmann <m.mathmann@simplicity.ag>
2023-05-04 16:19:08 +0200

    completed tutorial 5 - added products to index.html, so that you can click on collections and see the associated products

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from .models import *
 
 
@@ -11,3 +12,15 @@ def collections(request):
     category = Category.objects.filter(status=0)
     context = {'category': category}
     return render(request, "store/collections.html", context)
+
+
+def collectionsview(request, slug):
+    if (Category.objects.filter(slug=slug, status=0)):
+        products = Product.objects.filter(category__slug=slug)
+        category = Category.objects.filter(slug=slug).first()
+        context = {'products': products, 'category': category}
+        return render(request, 'store/products/index.html', context)
+    else:
+        messages.warning(request, "No such category found")
+        return redirect('collections')
+
